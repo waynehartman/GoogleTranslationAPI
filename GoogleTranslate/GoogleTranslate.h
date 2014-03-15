dab122b602a948756481e432d3d3397aa8548414
Wayne <waynehartman@gmail.com>
2014-03-15 09:59:58 -0500

    Initial Commit

---
+++ GoogleTranslate/GoogleTranslate.h
@@ -0,0 +1,13 @@
+//
+//  GoogleTranslate.h
+//  GoogleTranslate
+//
+//  Created by Wayne Hartman on 3/15/14.
+//  Copyright (c) 2014 Wayne Hartman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GoogleTranslate : NSObject
+
+@end
---
+++ GoogleTranslate/GoogleTranslate.m
@@ -0,0 +1,13 @@
+//
+//  GoogleTranslate.m
+//  GoogleTranslate
+//
+//  Created by Wayne Hartman on 3/15/14.
+//  Copyright (c) 2014 Wayne Hartman. All rights reserved.
+//
+
+#import "GoogleTranslate.h"
+
+@implementation GoogleTranslate
+
+@end
